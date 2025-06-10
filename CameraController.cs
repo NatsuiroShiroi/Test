efa747898495acc0e38b8f5c0609d87de96f507d
NatsuiroShiroi <104316291+NatsuiroShiroi@users.noreply.github.com>
2025-06-10 18:19:10 +0100

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 
+//Handles camera panning with WASD keys
 public class CameraController : MonoBehaviour
 {
-    [Tooltip("Units per second for the camera")]
-    public float cameraSpeed = 5f;
+    [Tooltip("Units per second for the camera")] public float cameraSpeed = 5f;
 
     void Update()
     {
-        Vector3 m = new Vector3(
-            Input.GetKey(KeyCode.D) ? +1 : Input.GetKey(KeyCode.A) ? -1 : 0,
-            Input.GetKey(KeyCode.W) ? +1 : Input.GetKey(KeyCode.S) ? -1 : 0,
-            0
-        );
-        if (m.sqrMagnitude > 1) m.Normalize();
-        transform.Translate(m * cameraSpeed * Time.deltaTime, Space.World);
+        HandleCameraMove();
+    }
+
+    private void HandleCameraMove()
+    {
+        float camMx = 0f, camMy = 0f;
+        if (Input.GetKey(KeyCode.W)) camMy += 1f;
+        if (Input.GetKey(KeyCode.S)) camMy -= 1f;
+        if (Input.GetKey(KeyCode.A)) camMx -= 1f;
+        if (Input.GetKey(KeyCode.D)) camMx += 1f;
+
+        Vector3 camMove = new Vector3(camMx, camMy, 0f);
+        if (camMove.sqrMagnitude > 1f) camMove.Normalize();
+        Camera.main.transform.Translate(camMove * cameraSpeed * Time.deltaTime, Space.World);
     }
 }
